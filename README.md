71769467d09a4bd898541ba425b78fa0f2ee66b6
vladimirovicp <vladimirovicp70@gmail.com>
2022-08-11 16:55:03 +0400

    add: скелет и стили

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # js_ToDoList
+
+## Создаём свой To Do List на JavaScript
+[ссылка на видео youtube](https://www.youtube.com/watch?v=VEA6_-gjPUY)
---
+++ main.js
@@ -0,0 +1,4 @@
+const addTaskBtn = document.getElementById("add-task-btn");
+const deskTaskInput = document.getElementById("description-task");
+const todosWrapper = document.querySelector('.todos-wrapper');
+
