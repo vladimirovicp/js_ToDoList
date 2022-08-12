4d7b36e9eb927a3fafc73872c1dadea566805dc6
vladimirovicp <vladimirovicp70@gmail.com>
2022-08-12 12:49:11 +0400

    v1

--- main.js
+++ main.js
@@ -2,3 +2,78 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const deskTaskInput = document.getElementById("description-task");
 const todosWrapper = document.querySelector('.todos-wrapper');
 
+let tasks;
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+let todoItemElems = [];
+
+const createTemplate = (task, index) => {
+    return `
+        <div class="todo-item ${task.completed ? 'checked' : ''}">
+            <div class="description">${task.description}</div>
+            <div class="buttons">
+                <input onclick="completeTask(${index})" class="btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
+                <button onclick="deleteTask(${index})" class="btn-delete">Delete</button>
+            </div>
+        </div>
+    `;
+}
+
+const filterTasks = () =>{
+    const activeTasks = tasks.length && tasks.filter(item => item.completed ==false);
+    const completedTasks = tasks.length && tasks.filter(item => item.completed ==true);
+    tasks = [...activeTasks,...completedTasks];
+}
+
+function  Task(description){
+    this.description = description;
+    this.completed = false;
+}
+
+const fillHtmlList = () =>{
+    todosWrapper.innerHTML = "";
+    if(tasks.length > 0){
+        filterTasks();
+        tasks.forEach((item, index) => {
+            todosWrapper.innerHTML +=createTemplate(item, index);
+        });
+        todoItemElems = document.querySelectorAll('.todo-item');
+    }
+}
+
+fillHtmlList();
+
+const updateLocal = () =>{
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const completeTask = index =>{
+    // console.log(index);
+    tasks[index].completed = !tasks[index].completed;
+
+    // console.log(tasks[index].completed);
+    if(tasks[index].completed){
+        todoItemElems[index].classList.add('checked');
+    } else{
+        todoItemElems[index].classList.remove('checked');
+    }
+    updateLocal();
+    fillHtmlList();
+}
+
+addTaskBtn.addEventListener('click', () =>{
+    tasks.push(new Task(deskTaskInput.value));
+    updateLocal();
+    fillHtmlList();
+    deskTaskInput.value='';
+})
+
+
+const deleteTask = index =>{
+    todoItemElems[index].classList.add('delition');
+    setTimeout(() => {
+        tasks.splice(index,1);
+        updateLocal();
+        fillHtmlList();
+    },500)
+}
+
